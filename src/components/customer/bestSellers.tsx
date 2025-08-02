1b737af7930c9cb00b42241ca18cb43d91890459
Shawan00 <VietAQ.B21CN787@stu.ptit.edu.vn>
2025-08-02 11:10:40 +0700

    fix deploy

--- src/components/customer/bestSellers.tsx
+++ src/components/customer/bestSellers.tsx
@@ -17,16 +17,16 @@ export default async function BestSellers() {
         <ProductCard product={products.data[0]} />
       </div>
       <div className="order-2 md:order-1 md:col-span-4 grid grid-cols-2 grid-rows-2 gap-3 xl:gap-8">
-        <ProductCard product={products.data[1]} />
-        <ProductCard product={products.data[1]} />
-        <ProductCard product={products.data[1]} />
-        <ProductCard product={products.data[1]} />
+        <ProductCard product={products.data[0]} />
+        <ProductCard product={products.data[0]} />
+        <ProductCard product={products.data[0]} />
+        <ProductCard product={products.data[0]} />
       </div>
       <div className="order-3 md:order-3 md:col-span-4 grid grid-cols-2 grid-rows-2 gap-3 xl:gap-8">
-        <ProductCard product={products.data[2]} />
-        <ProductCard product={products.data[2]} />
-        <ProductCard product={products.data[2]} />
-        <ProductCard product={products.data[2]} />
+        <ProductCard product={products.data[0]} />
+        <ProductCard product={products.data[0]} />
+        <ProductCard product={products.data[0]} />
+        <ProductCard product={products.data[0]} />
       </div>
     </div>
   )
